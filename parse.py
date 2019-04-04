8a003dcccc92ffe5a7b4a2f5dc2d4d841cb871a5
lsun <lsun@freewheel.tv>
2019-04-04 16:43:07 +0800

    retrieve component information

--- parse.py
+++ parse.py
@@ -1,7 +1,7 @@
 # encoding=utf8
 # python2.7
 
-# support IFC2X2_FINAL 
+# support IFC2X2_FINAL
 
 import ifcopenshell
 import sys
@@ -11,10 +11,11 @@ sys.setdefaultencoding('utf8')
 
 
 def print_obj(level, obj):
+    # print obj.get_info(), obj.is_a()
     if level == 0:
-        print obj.LongName
+        print obj.Name
     else:
-        print ','*level, obj.LongName
+        print ','*level, obj.Name
     # print '--'*level, obj.id(), obj.Name, obj.LongName, obj.is_a()
 
 
@@ -26,9 +27,19 @@ def get_children_from_aggr(obj):
     return children
 
 
+def get_component_from_spatial_struct(obj):
+    children = []
+    for ce in obj.ContainsElements:
+        for e in ce.RelatedElements:
+            children.append(e)
+    return children
+
+
 def traverse(obj, cur_lvl=0, max_lvl=0):
     print_obj(cur_lvl, obj)
     if cur_lvl == max_lvl:
+        for c in get_component_from_spatial_struct(obj):
+            print_obj(cur_lvl+1, c)
         return
     for child in get_children_from_aggr(obj):
         traverse(child, cur_lvl+1, max_lvl)
@@ -48,4 +59,4 @@ projects = fobj.by_type('IfcProject')
 
 print 'project, site, building, buildingstorey, space'
 for p in projects:
-    traverse(p, 0, 5)
+    traverse(p, 0, 3)
